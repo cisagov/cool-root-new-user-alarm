889bf2b4cf1371558db851cdca572949eb76373c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-09 10:11:00 -0400

    Add policy as an output

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "alarm" {
   value       = aws_cloudwatch_metric_alarm.new_user
   description = "The CloudWatch alarm that is triggered when a new user is created."
 }
+
+output "policy" {
+  value       = aws_iam_policy.provisionalarm
+  description = "The policy created to allow creation of the CloudWatch alarm."
+}
