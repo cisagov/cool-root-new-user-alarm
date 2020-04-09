1274b381ace76e4d3ad3387e62f0b9053dcc736c
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-09 11:11:09 -0400

    Add the metric filter to the outputs

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "alarm" {
   description = "The CloudWatch alarm that is triggered when a new user is created."
 }
 
+output "metric_filter" {
+  value       = aws_cloudwatch_log_metric_filter.new_user
+  description = "The metric filter that feeds the CloudWatch alarm."
+}
+
 output "policy" {
   value       = aws_iam_policy.provisionalarm
   description = "The policy created to allow creation of the CloudWatch alarm."
